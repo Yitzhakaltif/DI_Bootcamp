016f3b38bc902466fe38ab784bdf3f43684f9ebf
Yitzhakaltif <107390911+Yitzhakaltif@users.noreply.github.com>
2022-06-26 17:26:12 +0300

    Add files via upload

---
+++ Week2/Day1/DailyChallenge/script.js
@@ -0,0 +1,27 @@
+/*exercise 1:
+
+//Using this array :
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+//1.Remove Banana from the array.
+fruits.splice(0, 1);
+//2.Sort the array in alphabetical order.
+fruits.sort();
+//3.Add “Kiwi” to the end of the array.
+fruits.push("kiwi");
+//4.Remove “Apples” from the array. Don’t use the same method as in part 1.
+fruits.shift();
+
+//5.Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+fruits.reverse();
+*/
+
+/*exercise 2:
+
+//Using this array :
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+//1.Access and then console.log “Oranges”.
+console.log(moreFruits[1][1][0]);
+*/
