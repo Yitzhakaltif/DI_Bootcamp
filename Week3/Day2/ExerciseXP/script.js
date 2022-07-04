938cf3ff724b8849e99287c266b34b6f9e824f0f
Yitzhakaltif <isaacaltif123@gmail.com>
2022-07-04 14:16:40 +0300

    added new features

---
+++ Week3/Day2/ExerciseXP/script.js
@@ -0,0 +1,55 @@
+/*exercise: 1
+//1
+let a = document.getElementsByTagName('h1');
+console.log(a);
+//2
+let b = document.querySelectorAll('article')[0];
+b.removeChild(b.children[6]);
+console.log(b);
+//3
+function h2Click() {
+let c = document.querySelectorAll('h2')[0];
+c.style.backgroundColor = 'red';
+}
+//4
+function h3Click(){
+let d = document.querySelectorAll("h3")[0];
+d.style.display = "none";
+}
+//5
+
+function txtClick(){
+    let e = document.querySelectorAll("article")[0];
+    e.style.fontWeight = 'bold';
+    }
+*/
+
+/*Exercise: 3
+//1
+let = allBoldItems;
+//2
+function getBotld_items(){
+    let = allBoldItems;
+}
+//3
+function highlight(){
+    let blu = document.querySelectorAll('p')[4];
+    blu.style.color = 'blue';
+
+}
+//4
+function return_normal(){
+   let blck = document.querySelectorAll('p')[4];
+   blck.style.color = 'black';
+}
+highlight();
+return_normal();
+*/
+
+
+
+
+
+
+//change the onclicks to be in the js file only.
+//dont do exercises with forms and other things we didnt cover in class.
