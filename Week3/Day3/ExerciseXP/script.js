01c90f767b36dc9113ae4696d3828b46b3d69224
Yitzhakaltif <isaacaltif123@gmail.com>
2022-07-07 18:12:50 +0300

    adde new features

--- Week3/Day3/ExerciseXP/script.js
+++ Week3/Day3/ExerciseXP/script.js
@@ -3,8 +3,20 @@ let alrt = setTimeout(function() { alert("Hello World"); }, 2000);
 
 //2
 let para = document.getElementById("container");
+setTimeout( function(){
+    let p = document.createElement('p');
+    p.textContent ='Hello World';
+    para.appendChild(p)}, 2000);
 
-function getPara(){
-    para.createElement('p');
+//3
+let para2 = document.getElementById("container");
+let interval = setInterval(function p3(){
+ p2 = document.createElement('p');
+p2.textContent = 'Hello World';
+para2.appendChild(p2)
+}, 2000);
+
+function stop(){
+    clearInterval(interval);
 }
-getPara();
+document.getElementById("clear").addEventListener("click", stop);
