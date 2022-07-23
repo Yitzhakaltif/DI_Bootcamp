e32eb5c2878ee458607f8e866e4e447514be19e4
Yitzhakaltif <isaacaltif123@gmail.com>
2022-07-23 22:53:44 +0300

    Finished StarWars Project

--- Week5/Day5/ExerciseXP/script.js
+++ Week5/Day5/ExerciseXP/script.js
@@ -1,12 +1,4 @@
-// const xhr = new XMLHttpRequest();
-// const button = document.querySelector('#button');
-// // const names = document.querySelectorAll('#name');
-// const height = document.querySelector('#height');
-// const gender = document.querySelector('#gender');
-// const birthYear = document.querySelector('#birth-year');
-// const homeWorld = document.querySelector('#home-world');
 let displayDiv = document.getElementById("displayDiv");
-
 
 const getCharacter = async () => {
   displayDiv.innerHTML = `<i class="fas fa-spinner fa-spin fa-5x"></i>`
@@ -42,53 +34,4 @@ const display = (data) => {
 	<p>Home World: ${data.homeworld}</p>`;
 }
 
-// async function getInfo(){
-//     let randomNumber = Math.floor((Math.random()*88)+1)
-//     let apiUrl = `https://www.swapi.tech/api/people/${randomNumber}`;
-
-//     await fetch(apiUrl).then(function(response){
-//         updateInfo(response.data)
-//     })
-// }
-
-// function updateInfo(data){
-//     const names = document.querySelectorAll('#name');
-//     names.innerText = data.names
-// }
-
-// button.addEventListener(`click`, getInfo);
-
-
-
-
-
-
-
-
-// (function button() {
-//     const btn = document.getElementById('#button');
-//     btn.addEventListener("click");
-//     btn.innerText = "Find Someone";
-//     document.body.append(btn);
-//   })();
-
-
-// async function getData(){
-//     const API = await fetch(`https://www.swapi.tech/`);
-
-
-
-//     console.log(API);
-// }
-// getData();
-
-// const displayData = async ()=>{
-//     const response = await fetch(`https://www.swapi.tech/`);
-//     const jsonUserData = await response.json();
-// console.log(jsonUserData);
-//     return jsonUserData;
-
-// }
-
-// displayData();
 
