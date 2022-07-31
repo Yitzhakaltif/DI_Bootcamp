8ffc013dec90d3cb891a7c2278f412b8a9deb224
Yitzhakaltif <isaacaltif123@gmail.com>
2022-07-31 13:27:11 +0300

    added new features

---
+++ Week7/Day1/ExerciseXP/npm-node/main.js
@@ -0,0 +1,17 @@
+//exercise1 
+
+// let a = 5;
+
+// let b = 10;
+
+// console.log(`hello you are 15`);
+
+//exercise 2
+
+
+// console.log(`Hi, thanks for running me with npm start!`);
+
+//exercise 3
+
+
+
